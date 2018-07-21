90654d630a5d219eaabecd07dcfb095a1444d49e
tvermila <32718393+tvermila@users.noreply.github.com>
2018-07-21 22:42:59 +0300

    linted

--- index.js
+++ index.js
@@ -2,62 +2,31 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const nodemailer = require('nodemailer')
-const config = require('./config')
 const mailRouter = require('./controllers/mailController')
-require('dotenv').config()
+
 
 app.use(cors())
 app.use(bodyParser.json())
 
 const logger = (request, response, next) => {
-    console.log('Method:',request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-  }
+  console.log('Method:',request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
 app.use(logger)
 
 app.use('/api/mail', mailRouter)
 
-let notes = [
-    {
-      id: 1,
-      content: 'HTML on helppoa',
-      date: '2017-12-10T17:30:31.098Z',
-      important: true
-    },
-    {
-      id: 2,
-      content: 'Selain pystyy suorittamaan vain javascriptiä',
-      date: '2017-12-10T18:39:34.091Z',
-      important: false
-    },
-    {
-      id: 3,
-      content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
-      date: '2017-12-10T19:20:14.298Z',
-      important: true
-    }
-  ]
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/notes', (req, res) => {
-    res.json(notes)
-})
-
 const error = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
-  }
-  
-  app.use(error)
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(error)
 
 const port = 3001
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
